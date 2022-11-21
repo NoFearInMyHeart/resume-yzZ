b528e74e7e9fd3dbebf056282334e64058579125
zYz <zhuyueze>
2022-11-21 10:44:16 +0800

    导航栏修改

--- src/components/PcNav.tsx
+++ src/components/PcNav.tsx
@@ -6,9 +6,9 @@ interface Props {
 
 function PcNav({ nav }: Props) {
   return (
-    <div className="hidden lg:flex">
+    <div className="flex">
       {nav.map((item, index) => (
-        <a href={item.url} className="mr-10" key={index}>
+        <a href={item.url} className="ml-3 lg:ml-10" key={index}>
           {item.name}
         </a>
       ))}
